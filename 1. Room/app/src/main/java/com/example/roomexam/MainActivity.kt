8b9a3d5e8e9f4c30f996b99dad2e4ea127cb1778
김지현 <beee@Bui-Mac.local>
2020-09-21 13:43:59 +0900

    [비동기 처리]coroutine - lifecyclescope

--- 1. Room/app/src/main/java/com/example/roomexam/MainActivity.kt
+++ 1. Room/app/src/main/java/com/example/roomexam/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.roomexam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +18,14 @@ class MainActivity : AppCompatActivity() {
         //이렇게 작성시, db가 항상 백그라운드에서 동작하지 않으면 에러가 난다.
         // -> 메인스레드에서 db조작을 해도 괜찮게 allowMainThreadQueries()추가 --> 실무에서는 background 에서 동작하게
         val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "todo-db")
-            .allowMainThreadQueries().build()
+//            .allowMainThreadQueries()   앱의 성능이 느려질 수 있다. insert, delete등의 쿼리를 메인스레드에서 사용하면 안된다. (background에서 사용해야함.)
+            .build()
 
 
 //            //todoDao를 통해 데이터를 얻어올 수 있다.
 //            txt_result.text = db.todoDao().getAll().toString()
-//
-//
+
+
 //            //todo 추가하고 보여주기
 //            btn_todo.setOnClickListener {
 //                db.todoDao().insert(Todo(edit_todo.text.toString()))
@@ -33,10 +37,18 @@ class MainActivity : AppCompatActivity() {
             txt_result.text = it.toString()
         })
 
-        //버튼 클릭시 db에 insert
-        btn_todo.setOnClickListener {
-            db.todoDao().insert(Todo(edit_todo.text.toString()))
-        }
+//        //버튼 클릭시 db에 insert
+//        btn_todo.setOnClickListener {
+//            db.todoDao().insert(Todo(edit_todo.text.toString()))
+//        }
 
+        //비동기 처리를 해줄 것. - 버튼 클릭시 db에 insert
+        btn_todo.setOnClickListener {
+            //두가지 스레드가 있다. UI스레드, worker스레드 - 보이지 않는 db작업 위해 worker스레드 사용 위해 dispatcher사용 -- background에서 동작한다.
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao().insert(Todo(edit_todo.text.toString()))
+
+            }
+        }
     }
 }
